28786f3cb6cfc0dc7bea88a7dfdd3c8f9a2f4891
mariano <bertasaon@gmail.com>
2023-11-23 23:52:37 -0300

    commit entrega

--- JS/JS.js
+++ JS/JS.js
@@ -1,28 +1,8 @@
 function calcularFrecuenciaCambio(cantidadVacas, cantidadOrdeños, cantidadEquipos) {
     const frecuenciaOrdeños = 2500;
     const totalOrdeños = (cantidadVacas * cantidadOrdeños) / cantidadEquipos;
-    const frecuenciaDias = frecuenciaOrdeños / totalOrdeños ;
+    const frecuenciaDias = Math.round(frecuenciaOrdeños / totalOrdeños);
     return frecuenciaDias;
-}
-
-function obtenerNumero(mensaje) {
-    let input;
-    let esNumero = false;
-
-    while (!esNumero) {
-        input = prompt(mensaje);
-
-        if (input === null) {
-            return null; // Si el usuario presiona "Cancelar", retornar null
-        }
-
-        esNumero = !isNaN(input) && input.trim() !== ""; // Validar que sea un número y no esté en blanco
-        if (!esNumero) {
-            alert("Por favor, ingresa un número válido.");
-        }
-    }
-
-    return parseInt(input);
 }
 
 document.querySelector('.card form').addEventListener('submit', function(event) {
@@ -32,12 +12,12 @@ document.querySelector('.card form').addEventListener('submit', function(event) 
     const cantidadOrdeños = parseInt(document.getElementById("cantidadOrdeños").value);
     const cantidadEquipos = parseInt(document.getElementById("cantidadEquipos").value);
 
-    if (!isNaN(cantidadVacas) && !isNaN(cantidadOrdeños) && !isNaN(cantidadEquipos)) {
+    if (!isNaN(cantidadVacas) && !isNaN(cantidadOrdeños) && !isNaN(cantidadEquipos) && cantidadVacas > 0 && cantidadOrdeños > 0 && cantidadEquipos > 0) {
         const frecuenciaDias = calcularFrecuenciaCambio(cantidadVacas, cantidadOrdeños, cantidadEquipos);
 
-        alert(`El cambio óptimo en días es de: ${frecuenciaDias} días.`);
+        alert(`El cambio óptimo en días es de aproximadamente: ${frecuenciaDias} días.`);
     } else {
-        alert("Por favor, ingresa números válidos en todos los campos.");
+        alert("Por favor, ingresa números positivos y válidos en todos los campos.");
     }
 });
 
